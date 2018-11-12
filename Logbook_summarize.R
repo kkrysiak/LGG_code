dee1d6e4d7e8761803ddd5afdd7f59a259d90c7f
KK <kkrysiak@genome.wustl.edu>
2018-11-11 18:05:06 -0600

    add figures

--- Logbook_summarize.R
+++ Logbook_summarize.R
@@ -4,6 +4,9 @@
 ##########################################################################################################
 library(openxlsx)
 library(Amelia)
+library(ggplot2)
+library(gridExtra)
+library(stringr)
 
 setwd('~/Box Sync/LGG/Training/')
 
@@ -39,6 +42,18 @@ pdf(file = "missing_logbook_values.pdf", height=nrow(cases)/4, width=10)
   print(missmap(cases, y.labels = cases$`CoPath.#`))
 dev.off()
 
+## Remove PHI / unnecessary columns
+drop <- c("Entry","Date.collected","Date.Accessioned","Patient.name","DOB","MRN.#","Nomenclature")
+cases <- cases[,!(colnames(cases) %in% drop)]
+
+##########################################################################################################
+#### Get cases with 3 roles
+##########################################################################################################
+## Get a list of 7 required roles 
+seven <- req[which(req$section == "Role"),"main_section"]
+seven <- seven[!grepl("Roles|Written|Oral", seven)]
+
+three_roles <- cases[which(cases$Roles %in% seven),]
 
 ##########################################################################################################
 #### Create tables of logbook requirements
@@ -49,25 +64,25 @@ colnames(req) <- c("section", "main_section", "subsection", "min_req", "max_req"
 req[1,] <- c("Total", "Total", "-", "200", "0", length(unique(cases[which(cases$Results_summary %in% c("Normal","Abnormal")),"CoPath.#"])))
 
 ## Results
-sect <- "Results"
+sect <- "Total"
 main <- c("Normal","Abnormal")
-req <- rbind(req,c(sect, main[1], "-", 0, 100, length(unique(cases[which(cases$Results_summary == "Normal"),"CoPath.#"]))))
-req <- rbind(req,c(sect, main[2], "-", 100, 0, length(unique(cases[which(cases$Results_summary == "Abnormal"),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[1], "All", 0, 100, length(unique(cases[which(cases$Results_summary == "Normal"),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[2], "All", 100, 0, length(unique(cases[which(cases$Results_summary == "Abnormal"),"CoPath.#"]))))
 
 ## Testing category
 sect <- "Category"
 main <- lists$Testing.Category[!is.na(lists$Testing.Category)]
 sub <- c("NIPS","NIPS with confirmation","NIPS with patient results")
-req <- rbind(req,c(sect, main[1], "-", 30, 50, length(unique(cases[which(cases$Category == main[1]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[1], "All", 30, 50, length(unique(cases[which(cases$Category == main[1]),"CoPath.#"]))))
 req <- rbind(req,c(sect, main[1], sub[1], 3, 10, length(unique(cases[which(cases$Category == main[1] & cases$Sample.Type == "NIPT"),"CoPath.#"]))))
 req <- rbind(req,c(sect, main[1], sub[2], 1, 10, length(unique(cases[which(cases$Category == main[1] & cases$Testing.Subtype == "concomitant cytogenetics or molecular method"),"CoPath.#"]))))
 req <- rbind(req,c(sect, main[1], sub[3], 1, 10, length(unique(cases[which(cases$Category == main[1] & cases$Roles == "10. Oral communication - patients"),"CoPath.#"]))))
-req <- rbind(req,c(sect, main[2], "-", 40, 50, length(unique(cases[which(cases$Category == main[2]),"CoPath.#"]))))
-req <- rbind(req,c(sect, main[3], "-", 10, 50, length(unique(cases[which(cases$Category == main[3]),"CoPath.#"]))))
-req <- rbind(req,c(sect, main[4], "-", 10, 50, length(unique(cases[which(cases$Category == main[4]),"CoPath.#"]))))
-req <- rbind(req,c(sect, main[5], "-", 0, 20, length(unique(cases[which(cases$Category == main[5]),"CoPath.#"]))))
-req <- rbind(req,c(sect, main[6], "-", 0, 10, length(unique(cases[which(cases$Category == main[6]),"CoPath.#"]))))
-req <- rbind(req,c(sect, main[7], "-", 60, 0, length(unique(cases[which(cases$Category == main[7]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[2], "All", 40, 50, length(unique(cases[which(cases$Category == main[2]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[3], "All", 10, 50, length(unique(cases[which(cases$Category == main[3]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[4], "All", 10, 50, length(unique(cases[which(cases$Category == main[4]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[5], "All", 0, 20, length(unique(cases[which(cases$Category == main[5]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[6], "All", 0, 10, length(unique(cases[which(cases$Category == main[6]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[7], "All", 60, 0, length(unique(cases[which(cases$Category == main[7]),"CoPath.#"]))))
 sub <- c("Cytogenetic methods","Molecular methods")
 req <- rbind(req,c(sect, main[7], sub[1], 30, 50, length(unique(cases[which(cases$Category == main[7] & cases$Lab.Testing.Methods %in% c("1. G-banding","2. FISH","3. CMA")),"CoPath.#"]))))
 req <- rbind(req,c(sect, main[7], sub[2], 30, 50, length(unique(cases[which(cases$Category == main[7] & cases$Lab.Testing.Methods %in% c("4. Mutation Analysis","5. Sequence Analysis")),"CoPath.#"]))))
@@ -76,18 +91,18 @@ req <- rbind(req,c(sect, main[7], sub[2], 30, 50, length(unique(cases[which(case
 sect <- "Methodology"
 main <- lists$Testing.Methodology[!is.na(lists$Testing.Methodology)]
 sub <- lists$Testing.Method.Subcategory[!is.na(lists$Testing.Method.Subcategory)]
-req <- rbind(req,c(sect, main[1], "-", 50, 0, length(unique(cases[which(cases$Category == main[1]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[1], "All", 50, 0, length(unique(cases[which(cases$Category == main[1]),"CoPath.#"]))))
 req <- rbind(req,c(sect, main[1], sub[1], 0, 20, length(unique(cases[which(cases$Category == main[1] & cases$Testing.Subtype == "alone"),"CoPath.#"]))))
-req <- rbind(req,c(sect, main[2], "-", 30, 0, length(unique(cases[which(cases$Category == main[2]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[2], "All", 30, 0, length(unique(cases[which(cases$Category == main[2]),"CoPath.#"]))))
 req <- rbind(req,c(sect, main[2], sub[1], 0, 10, length(unique(cases[which(cases$Category == main[2] & cases$Testing.Subtype == "alone"),"CoPath.#"]))))
-req <- rbind(req,c(sect, main[3], "-", 30, 0, length(unique(cases[which(cases$Category == main[3]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[3], "All", 30, 0, length(unique(cases[which(cases$Category == main[3]),"CoPath.#"]))))
 req <- rbind(req,c(sect, main[3], sub[1], 0, 10, length(unique(cases[which(cases$Category == main[3] & cases$Testing.Subtype == "alone"),"CoPath.#"]))))
-req <- rbind(req,c(sect, main[4], "-", 20, 0, length(unique(cases[which(cases$Category == main[4]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[4], "All", 20, 0, length(unique(cases[which(cases$Category == main[4]),"CoPath.#"]))))
 mut <- sub[grepl(4, sub)]
 for(i in 1:length(mut)){
   req <- rbind(req,c(sect, main[4], mut[i], 5, 5, length(unique(cases[which(cases$Category == main[4] & cases$Testing.Subtype == mut[i]),"CoPath.#"]))))
 }
-req <- rbind(req,c(sect, main[5], "-", 40, 0, length(unique(cases[which(cases$Category == main[5]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[5], "All", 40, 0, length(unique(cases[which(cases$Category == main[5]),"CoPath.#"]))))
 req <- rbind(req,c(sect, main[5], "Sanger", 50, 0, length(unique(cases[which(cases$Category == main[5] & cases$Testing.Subtype == "Sequencing: Sanger [5.a]"),"CoPath.#"]))))
 req <- rbind(req,c(sect, main[5], "NGS", 0, 20, length(unique(cases[which(cases$Category == main[5] & grepl("NGS", cases$Testing.Subtype)),"CoPath.#"]))))
 
@@ -95,20 +110,70 @@ req <- rbind(req,c(sect, main[5], "NGS", 0, 20, length(unique(cases[which(cases$
 sect <- "Role"
 main <- lists$Roles[!is.na(lists$Roles)]
 sub <- c("Cases with 3 roles","Abnormal results")
-req <- rbind(req,c(sect, "Roles 1-7", "-", 100, 0, length(unique(cases[which(cases$Category %in% main[1:7]),"CoPath.#"]))))
+req <- rbind(req,c(sect, "Roles 1-7", "All", 100, 0, length(unique(cases[which(cases$Category %in% main[1:7]),"CoPath.#"]))))
 req <- rbind(req,c(sect, "Roles 1-7", sub[1], 180, 0, "TBD")) ############################### revisit
-req <- rbind(req,c(sect, main[1], "-", 0, 0, length(unique(cases[which(cases$Category == main[1]),"CoPath.#"]))))
-req <- rbind(req,c(sect, main[2], "-", 0, 0, length(unique(cases[which(cases$Category == main[2]),"CoPath.#"]))))
-req <- rbind(req,c(sect, main[3], "-", 0, 0, length(unique(cases[which(cases$Category == main[3]),"CoPath.#"]))))
-req <- rbind(req,c(sect, main[4], "-", 0, 0, length(unique(cases[which(cases$Category == main[4]),"CoPath.#"]))))
-req <- rbind(req,c(sect, main[5], "-", 0, 0, length(unique(cases[which(cases$Category == main[5]),"CoPath.#"]))))
-req <- rbind(req,c(sect, main[6], "-", 0, 0, length(unique(cases[which(cases$Category == main[6]),"CoPath.#"]))))
-req <- rbind(req,c(sect, main[7], "-", 0, 0, length(unique(cases[which(cases$Category == main[7]),"CoPath.#"]))))
-req <- rbind(req,c(sect, main[8], "-", 100, 0, length(unique(cases[which(cases$Category == main[8]),"CoPath.#"]))))
-req <- rbind(req,c(sect, main[9], "-", 10, 0, length(unique(cases[which(cases$Category == main[9]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[1], "All", 0, 0, length(unique(cases[which(cases$Category == main[1]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[2], "All", 0, 0, length(unique(cases[which(cases$Category == main[2]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[3], "All", 0, 0, length(unique(cases[which(cases$Category == main[3]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[4], "All", 0, 0, length(unique(cases[which(cases$Category == main[4]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[5], "All", 0, 0, length(unique(cases[which(cases$Category == main[5]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[6], "All", 0, 0, length(unique(cases[which(cases$Category == main[6]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[7], "All", 0, 0, length(unique(cases[which(cases$Category == main[7]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[8], "All", 100, 0, length(unique(cases[which(cases$Category == main[8]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[9], "All", 10, 0, length(unique(cases[which(cases$Category == main[9]),"CoPath.#"]))))
 req <- rbind(req,c(sect, main[9], sub[2], 5, 0, length(unique(cases[which(cases$Category == main[9] & cases$Results_summary == "Abnormal"),"CoPath.#"]))))
-req <- rbind(req,c(sect, main[10], "-", 20, 0, length(unique(cases[which(cases$Category == main[10]),"CoPath.#"]))))
+req <- rbind(req,c(sect, main[10], "All", 20, 0, length(unique(cases[which(cases$Category == main[10]),"CoPath.#"]))))
 req <- rbind(req,c(sect, main[10], sub[2], 10, 0, length(unique(cases[which(cases$Category == main[10] & cases$Results_summary == "Abnormal"),"CoPath.#"]))))
 
+req <- as.data.frame(req, stringsAsFactors=F)
+req$to_do <- as.numeric(req$min_req) - as.numeric(req$current)
+req[which(req$to_do < 0),"to_do"] <- 0
+req$section_label <- paste(req$main_section, "(", req$subsection,")")
+req$min_req <- as.numeric(req$min_req)
+req$max_req <- as.numeric(req$max_req)
+req$current <- as.numeric(req$current)
+req$requirement <- "Not met"
+req[which(req$current > req$min_req),"requirement"] <- "Met"
 
-summary(cases)
+## Define color palette
+color_palette=c("Not met"="red","Met"="green")
+
+## Reorder factors 
+req$section <- factor(req$section, levels=disord_list)
+
+tot <- ggplot(req[which(req$section == "Total"),], aes(section_label, max_req)) + geom_crossbar(aes(ymin = min_req, ymax = max_req), width = 0.2, fill="grey", linetype="blank") + geom_point(aes(section_label, current, color=requirement), size = 4) + labs(x="Total", y="Required") + scale_color_manual(values=color_palette) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + scale_x_discrete(labels = function(x) str_wrap(x, width = 25)) + theme(legend.position="none")
+
+cat <- ggplot(req[which(req$section == "Category"),], aes(section_label, max_req)) + geom_crossbar(aes(ymin = min_req, ymax = max_req), width = 0.2, fill="grey", linetype="blank") + geom_point(aes(section_label, current, color=requirement), size = 4) + labs(x="Category", y="Required") + scale_color_manual(values=color_palette) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + scale_x_discrete(labels = function(x) str_wrap(x, width = 30)) 
+
+meth <- ggplot(req[which(req$section == "Methodology"),], aes(section_label, max_req)) + geom_crossbar(aes(ymin = min_req, ymax = max_req), width = 0.2, fill="grey", linetype="blank") + geom_point(aes(section_label, current, color=requirement), size = 4) + labs(x="Methodology", y="Required") + scale_color_manual(values=color_palette) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + scale_x_discrete(labels = function(x) str_wrap(x, width = 30)) + theme(legend.position="none")
+
+role <- ggplot(req[which(req$section == "Role"),], aes(section_label, max_req)) + geom_crossbar(aes(ymin = min_req, ymax = max_req), width = 0.2, fill="grey", linetype="blank") + geom_point(aes(section_label, current, color=requirement), size = 4) + labs(x="Role", y="Required") + scale_color_manual(values=color_palette) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + scale_x_discrete(labels = function(x) str_wrap(x, width = 25)) + theme(legend.position="none")
+
+png(file = "logbook_status.png", height=1600, width=2200, res=150)
+  grid.arrange(tot,meth,role,cat, ncol=2, layout_matrix = rbind(c(1,2,2,2,2,2),c(3,3,3,4,4,4)))
+dev.off()
+
+
+
+
+# 
+# 
+# req$main <- gsub("\\S+\\. ","",req$main_section, perl=T)
+# req$main <- gsub(" \\(\\N+\\)","",req$main, perl=T)
+# req$main <- gsub(" - including digital image capture","",req$main, perl=T)
+# req$main <- gsub(" ","_",req$main, perl=T)
+# req$main <- gsub("-_","",req$main, perl=T)
+# req$main <- gsub("&_","",req$main, perl=T)
+# req$main <- gsub("G-","G_",req$main, perl=T)
+# req$main <- gsub("1-7","1to7",req$main, perl=T)
+# req$main <- gsub("1-7","1to7",req$main, perl=T)
+# req$sub <- gsub(" \\[\\S+\\.\\S+\\]","",req$subsection, perl=T)
+# 
+# ggplot(req[,c("sub","max_req","min_req","current","requirement","main")], aes(sub, max_req)) + geom_crossbar(aes(ymin = min_req, ymax = max_req), width = 0.2, fill="grey", linetype="blank") + geom_point(aes(sub, current, color=requirement), size = 4) + labs(x="Section", y="Required") + scale_color_manual(values=color_palette) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + facet_wrap(req$main)
+# 
+# ggplot(req, aes(sub, max_req)) + geom_crossbar(aes(ymin = min_req, ymax = max_req), width = 0.2, fill="grey", linetype="blank") + geom_point(aes(sub, current, color=requirement), size = 4) + labs(x="Section", y="Required") + scale_color_manual(values=color_palette) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + facet_wrap(req$main)
+# 
+# 
+# 
+# summary(cases)
+# summary(req)
